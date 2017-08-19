14dbb6470309c0ccd135798e08ce641523743a08
Corina <fluturelcorina@gmail.com>
2017-08-19 14:31:29 +0100

    Adding constants with key words in Score

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,6 +1,16 @@
 class Score < ApplicationRecord
+
+  STARTING_PRONOUNS = ["He", "She", "I", "It", "You", "My", "It's", "They", "They'll", "He'll", "She'll", "You'll"]
+  KEY_WORDS = ["sex", "things", "number", "you'll", "these", "believe", "tips", "tweets", "never", "photos", "photo", "best", "make", "just"]
+  KEY_PHRASES = ["here's why", "here's what", "you'll never", "that will", "this is", "how to", "the most", "what this", "when you", "and it's",
+                  "wait till", "this guy", "blow your", "the reason", "make you", "looks like", "pictures of"]
+
+
   def self.parse(score_params)
     # TODO: Parser logic goes here - current return is just for testing
     [1, 1]
   end
+
+
+
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Score, type: :model do
+
   it { is_expected.to have_db_column :vector }
   it { is_expected.to have_db_column :expected }
   it { is_expected.to have_db_column :result }
+
+  it 'has a list of starting pronouns' do
+    expect(Score::STARTING_PRONOUNS).to be
+  end
+
+  it 'has a list of key words' do
+    expect(Score::KEY_WORDS).to be
+  end
+
+  it 'has a list of key phrases' do
+    expect(Score::KEY_PHRASES).to be
+  end
+
 end
