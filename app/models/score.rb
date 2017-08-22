d545c261e9e65d177c4b9365d29088029c6d4c97
Alistair Kung <alistaircckung@gmail.com>
2017-08-22 17:12:52 +0100

    added parser metrics

--- app/models/score.rb
+++ app/models/score.rb
@@ -21,6 +21,10 @@ class Score < ApplicationRecord
     'looks like', 'pictures of'
   ].freeze
 
+  KEY_PRONOUNS = [
+    "i", "he", "she", "you", "my", "you'll", "me", "yours", "your", "you're", "mine", "their"
+  ].freeze
+
   def parse(score_params)
     raise "invalid query string" unless verify_params(score_params["title"])
     vector = create_vector(score_params['title'])
@@ -34,29 +38,46 @@ class Score < ApplicationRecord
     [
       MULTIPLIER,
       first_word(headline),
-      key_words(headline) + number_occurences(headline),
-      key_phrases(headline)
+      key_words(headline),
+      number_occurences(headline),
+      key_phrases(headline),
+      key_pronouns(headline)
     ]
   end
 
   def first_word(headline)
-    STARTING_PRONOUNS.inject(0) { |r, w| headline.split(' ')[0].to_i != 0 && r.zero? || headline.split(' ')[0] == w ? r + 1 : r }
+    STARTING_PRONOUNS.inject(0) { |r, w| headline.downcase.split(" ")[0].to_i != 0 &&
+    headline.downcase.split(" ")[0].to_i < 50 &&
+    r == 0 || headline.downcase.split(' ')[0] == w ? r + 1 : r }
   end
 
   def key_words(headline)
-    KEY_WORDS.inject(0) { |r, word| headline.downcase.include?(word) ? r + 1 : r }
+    KEY_WORDS.inject(0) { |r, word| headline.downcase.split(" ").include?(word) ? r + 1 : r }
   end
 
   def key_phrases(headline)
-    KEY_PHRASES.inject(0) { |r, phrase| headline.downcase.include?(phrase) ? r + 1 : r }
+    KEY_PHRASES.inject(0) { |r, phrase| headline.downcase.split(" ").include?(phrase) ? r + 1 : r }
+  end
+
+  def key_pronouns(headline)
+    KEY_PRONOUNS.reduce(0) { |r, word| headline.downcase.split(" ").include?(word) ? r + 1 : r }
   end
 
   def number_occurences(headline)
     headline.scan(/\d+/).count
   end
 
+  def is_i?(string)
+    /\A[-+]?\d+\z/ === string
+  end
+
+  def the_and_number(headline)
+    the_arr = headline.downcase.split(" ").each_index.select{|i| headline.downcase.split(" ")[i] == 'the'}
+    the_arr.inject(0){|r, item| is_i?(headline.downcase.split(" ")[item + 1]) ? r + 1 : r }
+  end
+
   def predict(vector)
-    perceptron = Perceptron.new(3)
+    perceptron = Perceptron.new(5)
     perceptron.predict(vector)
   end
 end
